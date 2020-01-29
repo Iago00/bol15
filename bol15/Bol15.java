e6debd8de4ddbeaa1c6e96ab94635ca35bb61179
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2020-01-29 10:35:04 +0100

    bol15

---
+++ bol15/Bol15.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol15;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Bol15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Academica alum1=new Academica("Luis",8,"9861823","laaaaa@aaa.com");
+        System.out.println(alum1.toString());
+    }
+    
+}
